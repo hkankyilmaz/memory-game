23c760b6d4f6062a9ef15d6da44803dedfe7a77d
Hakan KARAYILMAZ <99439329+hkankyilmaz@users.noreply.github.com>
2023-03-27 00:34:35 +0300

    window is not defined issue solved

--- frontend/components/gameDetail/GameDetail.tsx
+++ frontend/components/gameDetail/GameDetail.tsx
@@ -23,20 +23,12 @@ const renderTime = ({ remainingTime }: { remainingTime: number }) => {
 
 function GameDetail() {
   const [size, setSize] = React.useState(false);
-  window.addEventListener("resize", (event) => {
-    if (
-      typeof window !== "undefined" &&
-      window.innerWidth < 500 &&
-      size == false
-    )
-      setSize(true);
-    if (
-      typeof window !== "undefined" &&
-      window.innerWidth > 500 &&
-      size == true
-    )
-      setSize(false);
-  });
+  if (typeof window !== "undefined") {
+    window.addEventListener("resize", (event) => {
+      if (window.innerWidth < 500 && size == false) setSize(true);
+      if (window.innerWidth > 500 && size == true) setSize(false);
+    });
+  }
 
   return (
     <div className={styles.container}>
