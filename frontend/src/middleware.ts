ace2a1c49c450e0e3d2ef82e37070967961b6496
Hakan KARAYILMAZ <99439329+hkankyilmaz@users.noreply.github.com>
2023-04-13 22:26:06 +0300

    protect routes added -- nextjs middleware

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  // get cookie token
+  const hasToken = req.cookies.get("jwt");
+  console.log(hasToken);
+  console.log(req.url);
+
+  // protected routes (admin routes)
+  if (req.nextUrl.pathname.startsWith("/dashboard")) {
+    if (hasToken) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+  }
+
+  // login & register routes
+  if (["/login", "/register"].includes(req.nextUrl.pathname)) {
+    if (hasToken) {
+      return NextResponse.redirect(new URL("/dashboard", req.url));
+    } else {
+      return NextResponse.next();
+    }
+  }
+}
